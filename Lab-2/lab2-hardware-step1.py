a9958b7058c4434db593d208d9acf6f230f029a7
Oluwaseyi Sehinde-Ibini <seyisehindeibini@cmail.carleton.ca>
2020-10-09 14:01:04 -0400

    Updated file name to correct file name

---
+++ Lab-2/lab2-hardware-step1.py
@@ -0,0 +1,23 @@
+from sense_emu import SenseHat, ACTION_RELEASED, ACTION_PRESSED, ACTION_HELD
+
+sense = SenseHat()
+
+pressed = ACTION_PRESSED
+released = ACTION_RELEASED
+
+selection = False
+
+while True:
+	events = sense.stick.get_events()
+	if events:
+		for e in events:
+			if e.action == pressed:
+				if selection == False:
+					selection = True
+					sense.show_letter("O")
+                else:
+                    selection = False
+                    sense.show_letter("S")
+
+
+
---
+++ Lab-4/lab4.py
@@ -0,0 +1,14 @@
+import requests
+import urllib.request
+import json
+
+
+URL = 'https://api.thingspeak.com/update?api_key='
+KEY = '7481QW0APO2BO2BU"
+HEADER = '&field1={L1-F-2}&field2={d}&field3={seyisehindeibini@cmail.carleton.ca}'
+NEWURL = URL+KEY+HEADER
+print(NEWURL)
+
+DATA = url lib.request.urlopen(NEWURL)
+print(data)
+
